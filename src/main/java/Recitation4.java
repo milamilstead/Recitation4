d17fda5d574beb3bc6d780e7db9f180f86eb7f3f
milamilstead <mila.milstead@gmail.com>
2025-02-19 17:39:12 -0500

    COMPLETE!!!

--- src/main/java/Recitation4.java
+++ src/main/java/Recitation4.java
@@ -18,8 +18,21 @@ public class Recitation4 {
      */
     public static String explode(String word){
         StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length();i++){
+            char ch = word.charAt(i);
 
-        // TODO: replace this line with your code
+                if (Character.isLetter(ch)){
+                    for(int j = 0; j <= i ; j++){
+                        sb.append(ch);
+                    }
+                }else if(Character.isDigit(ch)){
+                    int num = Character.getNumericValue(ch);
+                    for (int s = 0; s < num; s++ )
+                    sb.append(ch);
+                }else {
+                    sb.append(word.substring(0,i));
+                }
+            }
 
         return sb.toString();
     }
